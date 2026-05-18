30010cc64c2ea53f591e48ca19461ec776e29d1f
Nathan Tuan <nathancyt2007@gmail.com>
2026-05-17 18:34:29 -0700

    initial backend deploy

---
+++ app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from ultralytics import YOLO
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# allow your GitHub Pages site to access this API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = YOLO("Yolo26nBEST.pt")
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    results = model(image, imgsz=640)
+
+    detections = []
+
+    for box in results[0].boxes:
+        detections.append({
+            "class": int(box.cls[0]),
+            "confidence": float(box.conf[0]),
+            "bbox": box.xyxy[0].tolist()
+        })
+
+    return {"detections": detections}
